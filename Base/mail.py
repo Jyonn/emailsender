c5c7b5beae5a3299160368b5251f110c3f9a367e
adelliu <lqj679ssn@qq.com>
2017-11-29 20:43:29 -0800

    change mail.py

--- Base/mail.py
+++ Base/mail.py
@@ -1,15 +1,9 @@
-import smtplib
-import email
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
 
-msg = email.message.Message()
-msg['From'] = 'noreply@mail.6-79.cn'
-msg['To'] = 'i@6-79.cn'
-msg['Subject'] = 'Test message'
-server = smtplib.SMTP('smtp.qq.com', 587)
-server.starttls()
-server.ehlo_or_helo_if_needed()
-try:
-    failed = server.sendmail('noreply@mail.6-79.cn', 'i@6-79.cn', msg.as_string())
-    server.close()
-except Exception as e:
-    print(e)
+msg = MIMEText("Here is the body of my message")
+msg["From"] = "noreply@mail.6-79.cn"
+msg["To"] = "i@6-79.cn"
+msg["Subject"] = "This is the subject."
+p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+p.communicate(msg.as_string())
