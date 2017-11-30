20c18702dcf256d08339011572a1616fa9ba22fc
adelliu <lqj679ssn@qq.com>
2017-11-29 20:46:05 -0800

    change mail.py

--- Base/mail.py
+++ Base/mail.py
@@ -1,9 +1,20 @@
-from email.mime.text import MIMEText
-from subprocess import Popen, PIPE
+# Import smtplib for the actual sending function
+import smtplib
 
-msg = MIMEText("Here is the body of my message")
-msg["From"] = "noreply@mail.6-79.cn"
-msg["To"] = "i@6-79.cn"
-msg["Subject"] = "This is the subject."
-p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-p.communicate(msg.as_string())
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+msg = MIMEText("just a text mail")
+
+# me == the sender's email address
+# you == the recipient's email address
+msg['Subject'] = 'The contents of code named strawberry'
+msg['From'] = 'noreply@mail.6-79.cn'
+msg['To'] = 'i@6-79.cn'
+
+# Send the message via our own SMTP server.
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
